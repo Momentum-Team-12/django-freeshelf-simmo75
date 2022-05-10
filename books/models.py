7e05d3657f49ee82ed355ef308fd0c2a3e616f16
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-05-10 10:06:11 -0700

    working-admin-add-books-and-book-info

--- books/models.py
+++ books/models.py
@@ -1,5 +1,3 @@
-from pickle import TRUE
-from turtle import title
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
@@ -18,3 +16,5 @@ class Book(models.Model):
     url = models.URLField(blank=True, null=True)
     description = models.TextField(max_length=500, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
